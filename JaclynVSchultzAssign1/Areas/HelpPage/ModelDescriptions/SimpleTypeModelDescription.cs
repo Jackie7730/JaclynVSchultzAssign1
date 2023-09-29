b58e913df5cb47dc1a8f167ca86731d1ef17105c
Jaclyn Schultz <jaclyn_schultz@hotmail.com>
2023-09-29 00:39:41 -0400

    Add project files.

---
+++ JaclynVSchultzAssign1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JaclynVSchultzAssign1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ JaclynVSchultzAssign1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JaclynVSchultzAssign1.WebApiApplication" Language="C#" %>
